567322d1dc64d195785f6ea4a2df46ae3cf96328
ankittanwar456 <gamerockerr@gmail.com>
2025-10-18 01:23:00 +0530

    Added: CI/CD

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+const config = {
+  appId: 'com.gamerockerr.currencyconvert',
+  appName: 'Currency Convert',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +8,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
